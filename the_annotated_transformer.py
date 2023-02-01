ce970a3383b4ab327605f6b8e9ab6c9aed118895
toeinriver <toeinriver@gmail.com>
2023-02-01 23:52:02 +0800

    Updates to run on a Macbook with M1 chip

--- the_annotated_transformer.py
+++ the_annotated_transformer.py
@@ -106,36 +106,80 @@
 # # !python -m spacy download en_core_web_sm
 
 
+import copy
+import math
 # %% id="v1-1MX6oTsp9"
 import os
-from os.path import exists
-import torch
-import torch.nn as nn
-from torch.nn.functional import log_softmax, pad
-import math
-import copy
 import time
-from torch.optim.lr_scheduler import LambdaLR
-import pandas as pd
-import altair as alt
-from torchtext.data.functional import to_map_style_dataset
-from torch.utils.data import DataLoader
-from torchtext.vocab import build_vocab_from_iterator
-import torchtext.datasets as datasets
-import spacy
-import GPUtil
 import warnings
-from torch.utils.data.distributed import DistributedSampler
+from os.path import exists
+
+import altair as alt
+import GPUtil
+import pandas as pd
+import spacy
+import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
+import torch.nn as nn
+import torchtext.datasets as datasets
+from torch.nn.functional import log_softmax, pad
 from torch.nn.parallel import DistributedDataParallel as DDP
-
+from torch.optim.lr_scheduler import LambdaLR
+from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
+from torchtext.data.functional import to_map_style_dataset
+from torchtext.vocab import build_vocab_from_iterator
 
 # Set to False to skip notebook execution (e.g. for debugging)
 warnings.filterwarnings("ignore")
 RUN_EXAMPLES = True
 
+# %% Test for Apple M1 architecture
 
+
+def test_mps():
+    dtype = torch.float
+    device = torch.device("mps")
+
+    # Create random input and output data
+    x = torch.linspace(-math.pi, math.pi, 2000, device=device, dtype=dtype)
+    y = torch.sin(x)
+
+    # Randomly initialize weights
+    a = torch.randn((), device=device, dtype=dtype)
+    b = torch.randn((), device=device, dtype=dtype)
+    c = torch.randn((), device=device, dtype=dtype)
+    d = torch.randn((), device=device, dtype=dtype)
+
+    learning_rate = 1e-6
+    for t in range(2000):
+        # Forward pass: compute predicted y
+        y_pred = a + b * x + c * x ** 2 + d * x ** 3
+
+        # Compute and print loss
+        loss = (y_pred - y).pow(2).sum().item()
+        if t % 100 == 99:
+            print(t, loss)
+
+        # Backprop to compute gradients of a, b, c, d with respect to loss
+        grad_y_pred = 2.0 * (y_pred - y)
+        grad_a = grad_y_pred.sum()
+        grad_b = (grad_y_pred * x).sum()
+        grad_c = (grad_y_pred * x ** 2).sum()
+        grad_d = (grad_y_pred * x ** 3).sum()
+
+        # Update weights using gradient descent
+        a -= learning_rate * grad_a
+        b -= learning_rate * grad_b
+        c -= learning_rate * grad_c
+        d -= learning_rate * grad_d
+
+    print(
+        f'Result: y = {a.item()} + {b.item()} x + {c.item()} x^2 + {d.item()} x^3')
+
+
+test_mps()
 # %%
 # Some convenience helper functions used throughout the notebook
 
@@ -368,6 +412,7 @@ class EncoderLayer(nn.Module):
     "Encoder is made up of self-attn and feed forward (defined below)"
 
     def __init__(self, size, self_attn, feed_forward, dropout):
+
         super(EncoderLayer, self).__init__()
         self.self_attn = self_attn
         self.feed_forward = feed_forward
